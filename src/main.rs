beeadc490062cb655d79ecbd1e188c5917052026
Praveen Senpai <pvnt20@gmail.com>
2026-08-06 21:08:58 +0530

    fix: use CARGO_PKG_VERSION macro instead of hardcoded version string

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use std::io;
 #[command(name = "agym")]
 #[command(author = "Praveensenpai")]
 #[command(disable_version_flag = true)]
-#[command(version = "0.6.12")]
+#[command(version = env!("CARGO_PKG_VERSION"))]
 #[command(about = "Unified Antigravity CLI & Account Manager", long_about = None)]
 struct Cli {
     /// Show the application version
